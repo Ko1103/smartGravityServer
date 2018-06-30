1b3920e52fed10ab58a9c5c15a87cf82d781325a
KO-YAMAKO <ko.foot1103@gmail.com>
2018-06-30 18:01:13 +0900

    create new model

--- model.go
+++ model.go
@@ -10,5 +10,12 @@ type Weight struct {
 	scaleId String //はかりのID
 	weight Int //重さ
 	createdAt time.Time //作成された時間
-	UpdatedAt time.Time //更新された時間
+	updatedAt time.Time //更新された時間
+}
+
+type Minimum struct {
+	id String //このオブジェクトのID
+	minimumWeight Int //はかりの下限値、これを下回った場合は購入を促す。
+	createdAt time.Time //作成された時間
+	updatedAt time.Time //更新された時間
 }
